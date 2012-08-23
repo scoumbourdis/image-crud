8188ab17b47c1ae4fdc8b65097c96465231359e2
scoumbourdis <scoumbourdisj@gmail.com>
2012-08-23 22:40:04 +0100

    Multilingual functionality created by: @noskov (Sergey Noskov).

--- application/libraries/image_crud.php
+++ application/libraries/image_crud.php
@@ -8,7 +8,7 @@
  */
 
 class image_CRUD {
-	
+
 	protected $table_name = null;
 	protected $priority_field = null;
 	protected $url_field = 'url';
@@ -22,155 +22,211 @@ class image_CRUD {
 	protected $views_as_string = '';
 	protected $css_files = array();
 	protected $js_files = array();
-	
+
+	protected $language = null;
+	protected $lang_strings = array();
+	protected $default_language_path = 'assets/image_crud/languages';
+
 	/**
-	 * 
+	 *
 	 * @var Image_moo
 	 */
 	private $image_moo = null;
-	
+
 	function __construct() {
 		$this->ci = &get_instance();
 	}
-	
+
 	function set_table($table_name)
 	{
 		$this->table_name = $table_name;
-		
+
 		return $this;
 	}
-	
+
 	function set_relation_field($field_name)
 	{
 		$this->relation_field = $field_name;
-		
+
 		return $this;
 	}
-	
+
 	function set_ordering_field($field_name)
 	{
 		$this->priority_field = $field_name;
-		
+
 		return $this;
 	}
-	
+
 	function set_primary_key_field($field_name)
 	{
 		$this->primary_key = $field_name;
 	}
-	
+
 	function set_subject($subject)
 	{
 		$this->subject = $subject;
-		
+
 		return $this;
 	}
-	
+
 	function set_url_field($url_field)
 	{
 		$this->url_field = $url_field;
-		
+
 		return $this;
 	}
-	
+
 	function set_title_field($title_field)
 	{
 		$this->title_field = $title_field;
-	
+
 		return $this;
-	}	
-	
+	}
+
 	function set_image_path($image_path)
 	{
 		$this->image_path = $image_path;
-		
+
 		return $this;
 	}
-	
+
 	function set_thumbnail_prefix($prefix)
 	{
 		$this->thumbnail_prefix = $prefix;
-		
+
 		return $this;
 	}
-	
+
 	public function set_css($css_file)
 	{
 		$this->css_files[sha1($css_file)] = base_url().$css_file;
 	}
-	
+
 	public function set_js($js_file)
 	{
 		$this->js_files[sha1($js_file)] = base_url().$js_file;
-	}	
-	
+	}
+
 	protected function _library_view($view, $vars = array(), $return = FALSE)
 	{
 		$vars = (is_object($vars)) ? get_object_vars($vars) : $vars;
-	
+
 		$file_exists = FALSE;
-	
+
 		$ext = pathinfo($view, PATHINFO_EXTENSION);
 		$file = ($ext == '') ? $view.'.php' : $view;
-	
+
 		$view_file = 'assets/image_crud/views/';
-	
+
 		if (file_exists($view_file.$file))
 		{
 			$path = $view_file.$file;
 			$file_exists = TRUE;
 		}
-	
+
 		if ( ! $file_exists)
 		{
 			throw new Exception('Unable to load the requested file: '.$file, 16);
 		}
-	
+
 		extract($vars);
-	
+
 		#region buffering...
 		ob_start();
-	
+
 		include($path);
-	
+
 		$buffer = ob_get_contents();
 		@ob_end_clean();
 		#endregion
-	
+
 		if ($return === TRUE)
 		{
 		return $buffer;
 		}
-	
+
 		$this->views_as_string .= $buffer;
-	}	
-	
+	}
+
 	public function get_css_files()
 	{
 		return $this->css_files;
 	}
-	
+
 	public function get_js_files()
 	{
 		return $this->js_files;
 	}
-	
+
+	/**
+	 *
+	 * Load the language strings array from the language file
+	 */
+	private function _load_language()
+	{
+		$ci = &get_instance();
+		$ci->config->load('image_crud');
+		if($this->language === null)
+		{
+			$this->language = strtolower($ci->config->item('image_crud_default_language'));
+		}
+		include($this->default_language_path.'/'.$this->language.'.php');
+
+		foreach($lang as $handle => $lang_string)
+			if(!isset($this->lang_strings[$handle]))
+				$this->lang_strings[$handle] = $lang_string;
+	}
+
+	/**
+	 *
+	 * Just an alias to get_lang_string method
+	 * @param string $handle
+	 */
+	public function l($handle)
+	{
+		return $this->get_lang_string($handle);
+	}
+
+	/**
+	 *
+	 * Get the language string of the inserted string handle
+	 * @param string $handle
+	 */
+	public function get_lang_string($handle)
+	{
+		return $this->lang_strings[$handle];
+	}
+
+	/**
+	 *
+	 * Simply set the language
+	 * @example english
+	 * @param string $language
+	 */
+	public function set_language($language)
+	{
+		$this->language = $language;
+
+		return $this;
+	}
+
 	protected function get_layout()
 	{
 		$js_files = $this->get_js_files();
 		$css_files =  $this->get_css_files();
-	
+
 		return (object)array('output' => $this->views_as_string, 'js_files' => $js_files, 'css_files' => $css_files);
 
-	}	
-	
-	protected function _upload_file($path) {  
+	}
+
+	protected function _upload_file($path) {
         $input = fopen("php://input", "r");
         $temp = tmpfile();
         $realSize = stream_copy_to_stream($input, $temp);
         fclose($input);
-        
-        $target = fopen($path, "w");        
+
+        $target = fopen($path, "w");
         fseek($temp, 0, SEEK_SET);
         stream_copy_to_stream($temp, $target);
         fclose($target);
@@ -182,10 +238,10 @@ class image_CRUD {
         		$this->image_moo->load($path)->resize(1024,768)->save($path,true);
         	}
         /* ------------------------------------- */
-        
+
         return true;
     }
-	
+
     protected function _changing_priority($post_array)
     {
     	$counter = 1;
@@ -194,13 +250,13 @@ class image_CRUD {
 			$this->ci->db->update($this->table_name, array($this->priority_field => $counter), array($this->primary_key => $photo_id));
 			$counter++;
 		}
-    }    
-	
+    }
+
     protected function _insert_title($primary_key, $value)
     {
 		$this->ci->db->update($this->table_name, array($this->title_field => $value), array($this->primary_key => $primary_key));
-    }    
-    
+    }
+
     protected function _insert_table($file_name, $relation_id = null)
     {
     	$insert = array($this->url_field => $file_name);
@@ -208,24 +264,24 @@ class image_CRUD {
     		$insert[$this->relation_field] = $relation_id;
     	$this->ci->db->insert($this->table_name, $insert);
     }
-    
+
     protected function _delete_file($id)
     {
     	$this->ci->db->where($this->primary_key,$id);
     	$result = $this->ci->db->get($this->table_name)->row();
-    	
+
     	unlink( $this->image_path.'/'.$result->{$this->url_field} );
     	unlink( $this->image_path.'/'.$this->thumbnail_prefix.$result->{$this->url_field} );
-    	
+
     	$this->ci->db->delete($this->table_name, array($this->primary_key => $id));
-    }    
-    
+    }
+
     protected function _get_delete_url($value)
     {
     	$rsegments_array = $this->ci->uri->rsegment_array();
     	return site_url($rsegments_array[1].'/'.$rsegments_array[2].'/delete_file/'.$value);
     }
-    
+
     protected function _get_photos($relation_value = null)
     {
     	if(!empty($this->priority_field))
@@ -237,35 +293,50 @@ class image_CRUD {
     		$this->ci->db->where($this->relation_field, $relation_value);
     	}
     	$results = $this->ci->db->get($this->table_name)->result();
-    	
+
     	$thumbnail_url = !empty($this->thumbnail_path) ? $this->thumbnail_path : $this->image_path;
-    	
+
     	foreach($results as $num => $row)
     	{
 			if (!file_exists($this->image_path.'/'.$this->thumbnail_prefix.$row->{$this->url_field})) {
 				$this->_create_thumbnail($this->image_path.'/'.$row->{$this->url_field}, $this->image_path.'/'.$this->thumbnail_prefix.$row->{$this->url_field});
-			}		
-		
+			}
+
     		$results[$num]->image_url = base_url().$this->image_path.'/'.$row->{$this->url_field};
     		$results[$num]->thumbnail_url = base_url().$this->image_path.'/'.$this->thumbnail_prefix.$row->{$this->url_field};
     		$results[$num]->delete_url = $this->_get_delete_url($row->{$this->primary_key});
     	}
-    	
+
     	return $results;
     }
-    
-    protected function _to_greeklish($str_i) {
-  
+
+    protected function _convert_greek_characters($str_i) {
+
 	  $g_c = array("α","ά","β","γ","δ","ε","έ","ζ","η","ή","θ","ι","ί","κ","λ","μ","ν","ξ","ο","ό","π","ρ","σ","ς","τ","υ","ύ","φ","χ","ψ","ω","ώ","Α","Ά","Β","Γ","Δ","Ε","Έ","Ζ","Η","Ή","Θ","Ι","Ί","Κ","Λ","Μ","Ν","Ξ","Ο","Ό","Π","Ρ","Σ","Τ","Υ","Ύ","Φ","Χ","Ψ","Ω","Ώ");
 	  $e_c = array("a","a","b","g","d","e","e","z","i","i","th","i","i","k","l","m","n","ks","o","o","p","r","s","s","t","u","u","f","x","y","w","w","A","A","B","G","D","E","E","Z","I","I","TH","I","I","K","L","M","N","KS","O","O","P","R","S","T","Y","Y","F","X","Y","W","W");
-	  
+
 	  for($i=0;$i<count($g_c);$i++){
 	      $str_i = str_replace($g_c[$i], $e_c[$i], $str_i);
 	  }
-	  
+
 	  return $str_i;
 	}
-    
+
+	// modified convert_accented_characters() function from text_helper
+	protected function _convert_foreign_characters($str_i)
+	{
+		//First of all convert greek characters as codeigniter doesn't support it yet.
+		$str_i = $this->_convert_greek_characters($str_i);
+		
+		//Next convert all the other kind of foreign characters
+		include(APPPATH.'config/foreign_chars.php');
+		if ( ! isset($foreign_characters))
+		{
+			return $str_i;
+		}
+		return preg_replace(array_keys($foreign_characters), array_values($foreign_characters), $str_i);
+	}
+
 	protected function _create_thumbnail($image_path, $thumbnail_path)
 	{
 		$this->image_moo
@@ -273,25 +344,25 @@ class image_CRUD {
 			->resize_crop(90,60)
 			->save($thumbnail_path,true);
 	}
-	
+
 	protected function getState()
 	{
 		$rsegments_array = $this->ci->uri->rsegment_array();
-		
+
 		if(isset($rsegments_array[3]) && is_numeric($rsegments_array[3]))
 		{
 			$upload_url = site_url($rsegments_array[1].'/'.$rsegments_array[2].'/upload_file/'.$rsegments_array[3]);
 			$ajax_list_url  = site_url($rsegments_array[1].'/'.$rsegments_array[2].'/'.$rsegments_array[3].'/ajax_list');
 			$ordering_url  = site_url($rsegments_array[1].'/'.$rsegments_array[2].'/ordering');
 			$insert_title_url  = site_url($rsegments_array[1].'/'.$rsegments_array[2].'/insert_title');
-			
+
 			$state = array( 'name' => 'list', 'upload_url' => $upload_url, 'relation_value' => $rsegments_array[3]);
 			$state['ajax'] = isset($rsegments_array[4]) && $rsegments_array[4] == 'ajax_list'  ? true : false;
 			$state['ajax_list_url'] = $ajax_list_url;
 			$state['ordering_url'] = $ordering_url;
 			$state['insert_title_url'] = $insert_title_url;
-			
-			
+
+
 			return (object)$state;
 		}
 		elseif( (empty($rsegments_array[3]) && empty($this->relation_field)) || (!empty($rsegments_array[3]) &&  $rsegments_array[3] == 'ajax_list'))
@@ -300,20 +371,21 @@ class image_CRUD {
 			$ajax_list_url  = site_url($rsegments_array[1].'/'.$rsegments_array[2].'/ajax_list');
 			$ordering_url  = site_url($rsegments_array[1].'/'.$rsegments_array[2].'/ordering');
 			$insert_title_url  = site_url($rsegments_array[1].'/'.$rsegments_array[2].'/insert_title');
-			
+
 			$state = array( 'name' => 'list', 'upload_url' => $upload_url);
 			$state['ajax'] = isset($rsegments_array[3]) && $rsegments_array[3] == 'ajax_list'  ? true : false;
 			$state['ajax_list_url'] = $ajax_list_url;
 			$state['ordering_url'] = $ordering_url;
 			$state['insert_title_url'] = $insert_title_url;
-			
+
 			return (object)$state;
 		}
 		elseif(isset($rsegments_array[3]) && $rsegments_array[3] == 'upload_file')
 		{
 			#region Just rename my file
 				$new_file_name = '';
-				$old_file_name = $this->_to_greeklish($_GET['qqfile']);
+				//$old_file_name = $this->_to_greeklish($_GET['qqfile']);
+				$old_file_name = $this->_convert_foreign_characters($_GET['qqfile']);
 				$max = strlen($old_file_name);
 				for($i=0; $i< $max;$i++)
 				{
@@ -324,12 +396,12 @@ class image_CRUD {
 					}
 					else
 					{
-						$new_file_name .= '-';	
+						$new_file_name .= '-';
 					}
 				}
 				$file_name = substr( substr( uniqid(), 9,13).'-'.$new_file_name , 0, 100) ;
 			#endregion
-			
+
 			$results = array( 'name' => 'upload_file', 'file_name' => $file_name);
 			if(isset($rsegments_array[4]) && is_numeric($rsegments_array[4]))
 			{
@@ -345,78 +417,79 @@ class image_CRUD {
 		elseif(isset($rsegments_array[3]) && $rsegments_array[3] == 'ordering')
 		{
 			$state = array( 'name' => 'ordering');
-			return (object)$state;			
+			return (object)$state;
 		}
 		elseif(isset($rsegments_array[3]) && $rsegments_array[3] == 'insert_title')
 		{
 			$state = array( 'name' => 'insert_title');
 			return (object)$state;
-		}		
-	}    
-    
+		}
+	}
+
 	function render()
 	{
 		$ci = &get_instance();
+		$this->_load_language();
 		$ci->load->helper('url');
 		$ci->load->library('Image_moo');
 		$this->image_moo = new Image_moo();
-		
+
 		$state_info = $this->getState();
-		
+
 		if(!empty($state_info))
 		{
 			switch ($state_info->name) {
 				case 'list':
 					$photos = isset($state_info->relation_value) ? $this->_get_photos($state_info->relation_value) : $this->_get_photos();
 					$this->_library_view('list.php',array(
-						'upload_url' => $state_info->upload_url, 
+						'upload_url' => $state_info->upload_url,
 						'insert_title_url' => $state_info->insert_title_url,
-						'photos' => $photos, 
+						'photos' => $photos,
 						'ajax_list_url' => $state_info->ajax_list_url,
 						'ordering_url' => $state_info->ordering_url,
 						'primary_key' => $this->primary_key,
 						'title_field' => $this->title_field,
 						'has_priority_field' => $this->priority_field !== null ? true : false
 					));
-					
+
 					if($state_info->ajax === true)
-					{	
+					{
 						echo $this->get_layout()->output;
 						die();
 					}
 					return $this->get_layout();
 				break;
-				
+
 				case 'upload_file':
 					$file_name = $state_info->file_name;
 					$this->_upload_file( $this->image_path.'/'.$file_name );
 					$this->_create_thumbnail( $this->image_path.'/'.$file_name , $this->image_path.'/'.$this->thumbnail_prefix.$file_name );
 					$this->_insert_table($file_name, $state_info->relation_value);
 					echo json_encode((object)array('success' => true));
-					
+
 					die();
-				break;	
+				break;
 
 				case 'delete_file':
 					$id = $state_info->id;
-					
+
 					$this->_delete_file($id);
-					
+
 					redirect($_SERVER['HTTP_REFERER']);
 				break;
-				
+
 				case 'ordering':
 					$this->_changing_priority($_POST['photos']);
 				break;
 
 				case 'insert_title':
 					$this->_insert_title($_POST['primary_key'],$_POST['value']);
-				break;				
+				break;
 			}
 		}
-		
+
 	}
-	
+
 }
 
 
--- assets/image_crud/views/list.php
+++ assets/image_crud/views/list.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 	$this->set_js('assets/image_crud/js/jquery-1.7.1.min.js');
 	$this->set_js('assets/image_crud/js/fileuploader.js');
 	$this->set_js('assets/image_crud/js/jquery-ui-1.8.13.custom.min.js');
 	$this->set_js('assets/image_crud/js//jquery.fancybox-1.3.4.pack.js');
 	$this->set_js('assets/image_crud/js/jquery.easing-1.3.pack.js');
 	$this->set_js('assets/image_crud/js/jquery.mousewheel-3.0.4.pack.js');
-	
+
 	$this->set_css('assets/image_crud/css/fileuploader.css');
 	$this->set_css('assets/image_crud/css/photogallery.css');
 	$this->set_css('assets/image_crud/css/ui/jquery-ui-1.8.13.custom.css');
@@ -21,10 +21,10 @@ function loadFancybox()
 	$('.fancybox').fancybox({
 		'transitionIn'	:	'elastic',
 		'transitionOut'	:	'elastic',
-		'speedIn'		:	600, 
-		'speedOut'		:	200, 
+		'speedIn'		:	600,
+		'speedOut'		:	200,
 		'overlayShow'	:	false
-	});	
+	});
 }
 function loadPhotoGallery(){
 	$.ajax({
@@ -33,7 +33,7 @@ function loadPhotoGallery(){
 		beforeSend: function()
 		{
 			$('.file-upload-messages-container:first').show();
-			$('.file-upload-message').html('Loading...');
+			$('.file-upload-message').html("<?php echo $this->l('loading');?>");
 		},
 		complete: function()
 		{
@@ -46,15 +46,27 @@ function loadPhotoGallery(){
 		}
 	});
 }
-function createUploader(){            
-    var uploader = new qq.FileUploader({
-        element: document.getElementById('file-uploader-demo1'),
-        action: '<?=$upload_url?>',
-        debug: true,
-        onComplete: function(id, fileName, responseJSON){
+function createUploader(){
+			var uploader = new qq.FileUploader({
+				element: document.getElementById('file-uploader-demo1'),
+				template: '<div class="qq-uploader">' +
+					'<div class="qq-upload-drop-area"><span><?php echo $this->l("upload-drop-area");?></span></div>' +
+					'<div class="qq-upload-button"><?php echo $this->l("upload_button");?></div>' +
+					'<ul class="qq-upload-list"></ul>' +
+					'</div>',
+				fileTemplate: '<li>' +
+					'<span class="qq-upload-file"></span>' +
+					'<span class="qq-upload-spinner"></span>' +
+					'<span class="qq-upload-size"></span>' +
+					'<a class="qq-upload-cancel" href="#"><?php echo $this->l("upload-cancel");?></a>' +
+					'<span class="qq-upload-failed-text"><?php echo $this->l("upload-failed");?></span>' +
+					'</li>',
+				action: '<?=$upload_url?>',
+				debug: true,
+				onComplete: function(id, fileName, responseJSON){
         	loadPhotoGallery();
         }
-    });           
+    });
 }
 function saveTitle(data_id, data_title)
 {
@@ -65,7 +77,7 @@ function saveTitle(data_id, data_title)
 			beforeSend: function()
 			{
 				$('.file-upload-messages-container:first').show();
-				$('.file-upload-message').html('Saving data...');
+				$('.file-upload-message').html("<?php echo $this->l('saving_title');?>");
 			},
 			complete: function()
 			{
@@ -87,15 +99,15 @@ function saveTitle(data_id, data_title)
 	<script type='text/javascript'>
 		$(function(){
 			$('.delete-anchor').click(function(){
-				if(confirm('Are you sure that you want to delete this image?'))
+				if(confirm('<?php echo $this->l("alert_delete");?>'))
 				{
 					$.ajax({
 						url:$(this).attr('href'),
 						beforeSend: function()
 						{
 							$('.file-upload-messages-container:first').show();
-							$('.file-upload-message').html('Deleting, please wait...');
-						},				
+							$('.file-upload-message').html("<?php echo $this->l('deleting');?>");
+						},
 						success: function(){
 							loadPhotoGallery();
 						}
@@ -107,24 +119,24 @@ function saveTitle(data_id, data_title)
 				return false;
 			});
     		$(".photos-crud").sortable({
-        		handle: '.move-box', 
-        		opacity: 0.6, 
-        		cursor: 'move', 
-        		revert: true,  
+        		handle: '.move-box',
+        		opacity: 0.6,
+        		cursor: 'move',
+        		revert: true,
         		update: function() {
     				var order = $(this).sortable("serialize");
 	    				$.post("<?=$ordering_url?>", order, function(theResponse){});
-    			}									  
+    			}
     		});
     		$('.ic-title-field').keyup(function(e) {
     			if(e.keyCode == 13) {
 					var data_id = $(this).attr('data-id');
 					var data_title = $(this).val();
-      			  	
+
 					saveTitle(data_id, data_title);
     			}
     		});
-    		
+
     		$('.ic-title-field').bind({
     			  click: function() {
     				$(this).css('resize','both');
@@ -138,7 +150,7 @@ function saveTitle(data_id, data_title)
 
 					var data_id = $(this).attr('data-id');
 					var data_title = $(this).val();
-      			  	
+
 					saveTitle(data_id, data_title);
     			  }
     		});
@@ -153,10 +165,10 @@ function saveTitle(data_id, data_title)
 					<textarea class="ic-title-field" data-id="<?php echo $photo->$primary_key; ?>" autocomplete="off" ><?php echo $photo->$title_field; ?></textarea>
 					<div class="clear"></div><?php }?>
 					<?php if($has_priority_field){?><div class="move-box"></div><?php }?>
-					<div class='delete-box'><a href='<?=$photo->delete_url?>' class='delete-anchor' tabindex="-1">Delete</a></div>
+					<div class='delete-box'><a href='<?=$photo->delete_url?>' class='delete-anchor' tabindex="-1"><?php echo $this->l('list_delete');?></a></div>
 					<div class="clear"></div>
 				</div>
-			</li> 
+			</li>
 	<?php }?>
 		</ul>
 		<div class='clear'></div>
