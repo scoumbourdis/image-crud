a6771ac9506d459e9df2a65ab12b9b5c9ca3e0a7
John Skoumbourdis <scoumbourdisj@gmail.com>
2013-02-15 22:08:58 +0000

    Adding Dutch language. A contribution from Frits Bosschert

---
+++ assets/image_crud/languages/dutch.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['upload_button'] = 'Upload bestanden hier';
+$lang['upload-drop-area'] = 'Sleep de bestanden hier naar toe om ze up te loaden';
+$lang['upload-cancel'] = 'Annuleren';
+$lang['upload-failed'] = 'Mislukt';
+
+$lang['loading'] = 'Laden, wacht alstublieft.. ';
+$lang['deleting'] = 'Verwijderen, wacht alstublieft.. ';
+$lang['saving_title'] = 'Opslaan titel.. ';
+
+$lang['list_delete'] = 'Verwijderen';
+$lang['alert_delete'] = 'Weet je zeker dat je deze afbeelding wilt verwijderen? ';
+
+/* End of file dutch.php */
+/* Location: ./assets/image_crud/languages/dutch.php */
