de192c34359b64812fdf7efefa78b5a5b58a38fd
CarlosPinedaT <carlospinedat@gmail.com>
2013-11-21 00:36:01 -0500

    Create spanish.php
    Spanish Translation of plugin

---
+++ assets/image_crud/languages/spanish.php
@@ -0,0 +1,16 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$lang['upload_button'] = 'Subir archivos aqui';
+$lang['upload-drop-area'] = 'Soltar archivos aqui para cargar';
+$lang['upload-cancel'] = 'Cancelar';
+$lang['upload-failed'] = 'Fallo';
+
+$lang['loading'] = 'Cargando, por favor espere ...';
+$lang['deleting'] = 'Eliminando, por favor espere ...';
+$lang['saving_title'] = 'Guardando titulo ...';
+
+$lang['list_delete'] = 'Eliminar';
+$lang['alert_delete'] = '¿Estás seguro de que quieres eliminar esta imagen?';
+
+/* End of file spanish.php */
+/* Location: ./assets/image_crud/languages/spanish.php */
